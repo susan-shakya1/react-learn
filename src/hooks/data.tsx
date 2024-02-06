668ef0364afa3bee3b1ea081094e98916456069f
susan-shakya1 <susanshakya2059@gmail.com>
2024-02-06 12:42:02 +0545

    useEffect life cycle method and conditional rendering

--- src/hooks/data.tsx
+++ src/hooks/data.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
+// Function based React lifecycle
+// 1. Component mount
+// 2. Component Unmount
 
+// Class based react lifecycle
+//ComponentDidMount
+//ComponentDidUpdate
+//ComponentWillUnmount
 export function Data() {
-  const [posts, setPost] = useState([]);
+  const [isPostVisible, setIsPostivisible] = useState(false);
+  const [posts, setPost] = useState<
+    { id: number; userId: number; body: string; title: string }[]
+  >([]);
   const fetchPost = () => {
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "GET",
@@ -9,7 +19,7 @@ export function Data() {
       .then((response) => {
         const parsed = response.json();
         parsed.then((data) => {
-          console.log("data from server", data);
+          // console.log("data from server", data);
           setPost(data);
         });
       })
@@ -18,10 +28,42 @@ export function Data() {
       });
   };
   useEffect(fetchPost, []);
+  const handlePostToggle = () => {
+    setIsPostivisible(!isPostVisible);
+  };
   return (
     <div>
-      <h1>Data loading from backend</h1>
-      <div>{JSON.stringify(posts)}</div>
+      <h1
+        style={{
+          margin: "16px 0",
+        }}
+      >
+        Posts
+      </h1>
+      <div>
+        <button onClick={handlePostToggle}>Toggle Posts</button>
+      </div>
+      {isPostVisible ? (
+        <div>
+          {posts.map((post) => {
+            return (
+              <div
+                key={post.id}
+                style={{
+                  border: "1px solid #ccc",
+                  padding: "16px",
+                  boxShadow: "1px 2px 3px #eee",
+                }}
+              >
+                <h2>{post.title}</h2>
+                <p>{[post.body]}</p>
+              </div>
+            );
+          })}{" "}
+        </div>
+      ) : (
+        <p>Post is not visible</p>
+      )}
     </div>
   );
 }
